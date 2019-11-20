5c0cc6da5b18f091b0a0dc5c65f68742440bd7e1
kuppireddigari swetha <kuppireddyswetha@gmail.com>
2019-11-20 20:56:18 +0530

    Swagger Implementation

--- ProductsApp/src/main/java/com/example/productsapp/ProductController.java
+++ ProductsApp/src/main/java/com/example/productsapp/ProductController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 	 @Autowired
 	    private ProductsRepository productRepository;
-	 
+	  
 	 @Autowired
 	    private SubProductRepository subProductRepository;
 	 
